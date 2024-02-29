34c6e2c85c9d9a6d75dd31b6c49a55be7c13eb23
jugalkkt <jugalkakkat@gmail.com>
2024-02-29 09:45:42 +0530

    Make grid visible

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let container = document.querySelector('.container');
+let rows = 16; let cols = 16;
+document.addEventListener('DOMContentLoaded', function()
+{
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++)
+        {
+            let gridItem = document.createElement('div');
+            gridItem.style.left = i*100 + "px";
+            gridItem.style.top = j*100 + "px";
+            gridItem.classList.add('gridItem')
+            container.appendChild(gridItem);
+        }
+    }
+})
