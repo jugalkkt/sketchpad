49a62205605b0f30fedb387b95deb270a47f9169
jugalkkt <jugalkakkat@gmail.com>
2024-03-01 09:52:12 +0530

    Fix gridItems dynamic dimensions issue
    The gridItems were not filling in rhe container, it is fixed now

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let container = document.querySelector('.container');
 let numGrid = prompt("Enter number of grids: ");
+if (numGrid > 100) numGrid = 100;
 const GRIDSIZE = 900;
 document.addEventListener('DOMContentLoaded', function()
 {
@@ -8,9 +9,11 @@ document.addEventListener('DOMContentLoaded', function()
         for (let j = 0; j < numGrid; j++)
         {
             let gridItem = document.createElement('div');
-            gridItem.style.width = gridItem.style.height = `${(GRIDSIZE / numGrid) - 2}px`;
-            gridItem.style.left = i*100 + "px";
-            gridItem.style.top = j*100 + "px";
+            let gridSizePx = (GRIDSIZE / numGrid);
+            gridItem.style.width = `${gridSizePx}` + 'px';
+            gridItem.style.height = `${gridSizePx}` + 'px';
+            gridItem.style.left = `${(i * gridSizePx)}` + 'px';
+            gridItem.style.top = `${(j * gridSizePx)}` + 'px';
             gridItem.classList.add('gridItem')
             container.appendChild(gridItem);
             gridItem.addEventListener( 'mouseenter', function()
